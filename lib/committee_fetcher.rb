e73b552168f5e27aa11712dce1168f04284544cf
Chris McGrath <chris@octopod.info>
2011-11-05 20:36:50 +0000

    First pass that gets the data
      * Problems with committees with & in their name

--- lib/committee_fetcher.rb
+++ lib/committee_fetcher.rb
@@ -1,6 +1,62 @@
+require 'net/http'
 require 'nokogiri'
 require 'uri'
 PARLIAMENT_SITE = 'data.parliament.uk'
+
+class Lord
+  attr_reader :id, :name
+
+  def initialize(xml)
+    @id = xml.attr('id')
+    @name = [
+      xml.at_xpath('m:rank').text,
+      xml.at_xpath('m:firstName').text,
+      xml.at_xpath('m:lastName').text
+    ].join(' ')
+  end
+
+  def to_s
+    name
+  end
+end
+
+class Member
+  attr_reader :id, :name
+
+  def self.members
+    @members ||= MemberFetcher.fetch_all
+  end
+
+  def self.fetch(id)
+    members[id]
+  end
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+
+  def to_s
+    name
+  end
+end
+
+class MemberFetcher
+  def self.fetch_all
+    members = {}
+    Nokogiri.XML(make_http_request('/resources/members/api/commons/')).xpath('//m:commonsMember').each do |member_xml|
+      id = member_xml.attr('id')
+      first_name = member_xml.at_xpath('m:firstName').text
+      last_name = member_xml.at_xpath('m:lastName').text
+      members[id] = Member.new(id, "#{first_name} #{last_name}")
+    end
+    members
+  end
+
+  def self.make_http_request(path)
+    Net::HTTP.get_response(PARLIAMENT_SITE, path, 80).body
+  end
+end
 
 class Committee
   attr_reader :id, :name, :committee_type
@@ -18,6 +74,25 @@ class Committee
   def commons_member_path
     "/resources/members/api/committees/#{URI.escape(name)}/#{id}/members/commons"
   end
+
+  def members
+    lords = Nokogiri.XML(make_http_request(lords_member_path)).xpath('//m:peer').map do |lord_xml|
+      Lord.new(lord_xml)
+    end
+
+    commons = Nokogiri.XML(make_http_request(commons_member_path)).xpath('//m:commonsMember').map do |member_xml|
+      Member.fetch(member_xml.attr('id'))
+    end
+
+    lords + commons
+  rescue
+    []
+  end
+
+private
+  def make_http_request(path)
+    Net::HTTP.get_response(PARLIAMENT_SITE, path, 80).body
+  end
 end
 
 class CommitteeFetcher
@@ -27,10 +102,6 @@ class CommitteeFetcher
     xml.xpath('//c:committee').map do |committee_xml|
       Committee.new(committee_xml)
     end
-  end
-
-  def self.create_committee(xml)
-
   end
 
 private
