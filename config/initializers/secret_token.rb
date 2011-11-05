57be5baa544fcc6b545052a31cd9571db82239ab
Chris McGrath <chris@octopod.info>
2011-11-05 15:45:40 +0000

    Initial commit of rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Committee::Application.config.secret_token = 'a68a8c204df85009e7736b87bf4737e3f70fa68fc3a9949710337c9d5097435c7c7ec7667706c363099c91e932fe0887b09c8c11dfaf3a8cdff4583f0be20509'
